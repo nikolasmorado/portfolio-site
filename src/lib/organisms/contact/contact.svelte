e2c38d2099ec9a13ace301cb20c2433c2837e50a
nikolas morado <nmorado2003@gmail.com>
2022-09-22 21:28:18 -0400

    Accidentally deleted contact file

---
+++ src/lib/organisms/contact/contact.svelte
@@ -0,0 +1,11 @@
+<script>
+    import ConsoleOutput from "../../molecules/consoleOutput/consoleOutput.svelte"
+    import ConsoleTaskbar from "../../molecules/consoleTaskbar/consoleTaskbar.svelte";
+</script>
+
+<style>
+    @import './contact.css';
+</style>
+
+<ConsoleTaskbar />
+<ConsoleOutput />
